6ce0ebee609078e22c5ce6e7839afba239172ec4
Mario.Koco <mario.koco92@gmail.com>
2021-04-16 16:52:14 +0200

    inserita costante con elemento del html, inserita funzione che stampi ogni icona su pagina, tramiter ciclo forEach, destrutturati gli elementi di un oggetto e sostituiti per ogni icona.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	// Array di oggetti con le proprietà delle icone, importato dall'esterno.
 	const icons = [
 		{
 			name: 'cat',
@@ -98,11 +99,52 @@ $(document).ready(function() {
 		}
 	];
 
+	// Array con elementi , importato dall'esterno, con elementi guida per assegnazione
+	// dei colori per le icone.
 	const colors = [
 		'blue',
 		'orange',
 		'purple'
 	];
+
+	// Salvo in una costante l'elemento del html che mi occorre per popolarlo di icone.
+	const iconsContainer = $("#icon_container");
+	
+	// / * ---------- Function Call --------- * / //
+	printIcons(icons, iconsContainer);
+
+
+
+
+
+	// / * ---------- Function ---------- * / //
+
+	// Funzione che tampa le icone su pagina HTML.
+	// 
+	// iconsArray ==> è l'array di oggetti che contiene le icone.
+	// container ==> è l'oggetto jQuery con il container che racchiude ogni singola icona sull'HTML.
+	function printIcons(iconsArray, container) {
+		// Ciclo l'array delle icone con "ForEach".
+		iconsArray.forEach((element) => {
+			// Inizio destrutturando element per leggere le informazioni di ogni singolo oggetto.
+			const {name, prefix, family} = element;
+			// Salvo in una costante il backtick che verrà stamapato in pagina HTML.
+			// Con la destrutturazione fatta in precedenza, sostituisco gli elementi della classe 
+			// presenti, con ogni elemento dell'oggetto delle icone.
+			const iconElementHtml = `
+				<div id="icon_container" class="icon"> 
+					<i class="${family} ${prefix}${name}"></i>
+					<div>
+						${name.toUpperCase()}
+					</div>
+				</div>
+			`;
+			// Con questa funzione "appenderò" ogni singolo elemento dell'ogeeto icons e le inserirò
+			// nel suo container nella pagina HTML.
+			container.append(iconElementHtml);
+		});
+	}
+
 
 	// Milestone 1
 	// Partendo dalla seguente struttura dati , 
